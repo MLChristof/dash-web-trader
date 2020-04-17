eb805c94197504a7f8698be3138fae0e4b9af50b
christofw <christof.westland@gmail.com>
2020-04-17 11:42:57 +0200

    logo change

--- app.py
+++ app.py
@@ -795,7 +795,7 @@ app.layout = html.Div(
                     className="div-info",
                     children=[
                         html.Img(
-                            className="logo", src=app.get_asset_url("dash-logo-new.png")
+                            className="logo", src=app.get_asset_url("multilul.png")
                         ),
                         html.H6(className="title-header", children="FOREX TRADER"),
                         html.P(
